e3edf244c95bff30fd60826ae984df9337b8b7d2
Marcel Stimberg <marcel.stimberg@inserm.fr>
2016-02-02 15:50:47 +0100

    Setup testing for travis

--- dev/conda-recipe/build.sh
+++ dev/conda-recipe/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cp -r $RECIPE_DIR/../../brian2tools $SRC_DIR
 cp $RECIPE_DIR/../../setup.py $SRC_DIR
+cp $RECIPE_DIR/../../README.rst $SRC_DIR
 $PYTHON $SRC_DIR/setup.py install --single-version-externally-managed --record=record.txt
