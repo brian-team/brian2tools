71fa0110c685146c978afa1a95db00701bb0dab4
Marcel Stimberg <marcel.stimberg@inserm.fr>
2016-05-12 15:57:33 +0200

    Fix a Python 3-incompatible import

--- brian2tools/__init__.py
+++ brian2tools/__init__.py
@@ -7,7 +7,7 @@ from .tests import run as test
 
 try:
     # Use version written out by setuptools
-    from version import version
+    from .version import version
     __version__ = version
 except ImportError:
     # Apparently we are running directly from a git clone, let
