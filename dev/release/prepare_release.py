55cbfa685f3a3257a889d411f5e7cfe9d32a2b3f
Marcel Stimberg <marcel.stimberg@inserm.fr>
2017-06-13 18:03:26 +0200

    Fix path to setup.py in release script and do a commit with updated setup.py

--- dev/release/prepare_release.py
+++ dev/release/prepare_release.py
@@ -15,9 +15,12 @@ if ret_val != 0:
 print('Current version is: ' + brian2tools.__version__)
 version = raw_input('Enter new Brian2 version number: ').strip()
 # update setup.py
-setup_py = open('setup.py', 'r').read()
+setup_py = open('../../setup.py', 'r').read()
 setup_py = re.sub("version\s*=\s*'.*?'", "version='" + version + "'", setup_py)
-open('setup.py', 'w').write(setup_py)
+open('../../setup.py', 'w').write(setup_py)
+
+# commit
+os.system('git commit -a -v -m "***** Release brian2tools %s *****"' % version)
 
 # add tag
 os.system('git tag -a -m "Release brian2tools %s" %s' % (version, version))
