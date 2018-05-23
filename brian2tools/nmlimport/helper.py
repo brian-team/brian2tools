92f21af3d7b402c3ef335c989cefed85d545ba14
kapil <kapilkd13@gmail.com>
2018-05-23 20:16:14 +0530

    initial commit: creating nmlimport library

---
+++ brian2tools/nmlimport/helper.py
@@ -0,0 +1,44 @@
+from pprint import pformat
+
+# return segment type depending on proximal and distal diameter
+def get_segment_type(segment=None):
+    if segment.proximal.diameter==segment.distal.diameter:
+        return "cylinder"
+    return "section"
+
+
+# get parent segment
+def get_parent_segment(segment,segments):
+    for s in segments:
+        if s.id==segment.parent.segments:
+            return s
+
+
+# check if distal of first segment joins with proximal of next segment
+def are_segments_joined(segment1,segment2):
+    if segment2.parent==None:
+        if segment1.proximal.x==segment2.proximal.x \
+                and segment1.proximal.y==segment2.proximal.y \
+                and segment1.proximal.z==segment2.proximal.z:
+            return True
+    return segment1.proximal.x==segment2.distal.x \
+           and segment1.proximal.y==segment2.distal.y \
+           and segment1.proximal.z==segment2.distal.z
+
+
+# shift coordinates for further processing
+def shiftCoords(x):
+    if x:
+        if x[0]:
+            x[:]= [a - x[0] for a in x]
+        else:
+             x[:]= [a + x[0] for a in x]
+    return x
+
+
+# pretty format data
+def formatter(datum):
+    a = pformat(datum)
+    if len(a) > 160:
+        a = a[0:160] + "[...]"
+    return a
---
+++ brian2tools/nmlimport/nml.py
@@ -0,0 +1,122 @@
+import neuroml.loaders as loaders
+from neuroml.utils import validate_neuroml2
+from brian2 import *
+from brian2.utils.logger import get_logger
+from os.path import abspath
+from .helper import *
+
+logger = get_logger(__name__)
+
+
+class ValidationException(Exception):
+    pass
+
+
+# Validating morphology segments positioning
+def validate_morphology(segments):
+    try:
+        start_segment = None
+        for segment in segments:
+            if segment.parent != None:
+                seg_parent = get_parent_segment(segment, segments)
+
+                if not are_segments_joined(segment, seg_parent):
+                    raise ValidationException("{0} and its parent "
+                                              "segment {1} are not connected!!".
+                                              format( formatter(segment),
+                                                      formatter(seg_parent)))
+
+            elif start_segment != None:
+                raise ValidationException("Two segments with parent id "
+                                          "as -1 (i.e no parent): {0} and {1}".
+                                          format(formatter(start_segment)
+                                                     , formatter(segment)))
+            else:
+                start_segment = segment
+                logger.info("starting segment is {0}".format(formatter(segment)))
+
+    except ValidationException as v:
+        logger.error("Validation error: {0}".format(v))
+        raise
+
+    except Exception as e:
+        logger.error("Exception occured: {0}".format(e))
+        raise
+
+
+# generate morphology object from given cell
+def generate_morph_object(cell):
+
+    # sort segment list in ascending order of id
+    sorted_segments = sorted(cell.morphology.segments, key=lambda x: x.id)
+    logger.info("Sorted segments are: {0}".format(formatter(sorted_segments)))
+
+    # tuple generation
+    points = ()
+
+    # validate morphology
+    validate_morphology(sorted_segments)
+
+    # generate initial tuple
+    seg = sorted_segments[0]
+    point = (seg.id, seg.name, seg.proximal.x \
+                 , seg.proximal.y, \
+             seg.proximal.z, seg.proximal.diameter, \
+             -1 if seg.parent == None else seg.parent.segments)
+    points = points + (point,)
+
+    # iterate over morphology segments
+    for segment in sorted_segments:
+        segment_type = get_segment_type(segment)
+        point = (segment.id + 1, segment.name, segment.distal.x \
+                     , segment.distal.y, \
+                 segment.distal.z, segment.distal.diameter, \
+                 0 if segment.parent == None else segment.parent.segments + 1)
+        points = points + (point,)
+
+    logger.info("Sequence of points sent to `from_points` function: \n{0}"
+                .format(formatter(points)))
+
+    return Morphology.from_points(points, spherical_soma=False)
+
+
+# return morphology object present in cell specified by cell_id
+def load_morph_from_cells(cells, cell_id=None):
+    if cell_id == None:
+        return generate_morph_object(cells[0])
+    for cell in cells:
+        if (cell_id == cell.id):
+            return generate_morph_object(cell)
+
+    err= ("The cell id you specified {0} doesn't exist."
+          " Present cell id's are:\n {1}".format( formatter(cell_id),
+          formatter([cell.id for cell in cells])))
+    logger.error('Value error: %s'% err)
+
+    raise ValueError(err)
+
+
+# returns final morphology object
+def load_morphology(nml_file, cell_id=None):
+
+    # Generate absolute path if not provided already
+    nml_file = abspath(nml_file)
+
+    # Validating NeuroML file
+    validate_neuroml2(nml_file)
+    logger.info("Validated provided .nml file")
+
+    # Return morphology
+    doc = loaders.NeuroMLLoader.load(nml_file)
+    logger.info("Loaded morphology file from: {0}".format(nml_file))
+
+    if size(doc.cells) > 1:
+        if cell_id != None:
+            return load_morph_from_cells(doc.cells, cell_id=cell_id)
+        morphologies = {}
+        for cell in doc.cells:
+            morphologies[cell.id] = load_morph_from_cells(doc.cells, cell_id=cell.id)
+        return morphologies
+
+    elif size(doc.cells) == 1:
+        return load_morph_from_cells(doc.cells, cell_id=cell_id)
