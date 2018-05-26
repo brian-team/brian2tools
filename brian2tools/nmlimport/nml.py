23ca31b0f34b237837d2a7da3b998f07056d698f
kapil <kapilkd13@gmail.com>
2018-05-26 16:02:41 +0530

    added get_points function

--- brian2tools/nmlimport/nml.py
+++ brian2tools/nmlimport/nml.py
@@ -23,17 +23,18 @@ def validate_morphology(segments):
                 if not are_segments_joined(segment, seg_parent):
                     raise ValidationException("{0} and its parent "
                                               "segment {1} are not connected!!".
-                                              format( formatter(segment),
-                                                      formatter(seg_parent)))
+                                              format(formatter(segment),
+                                                     formatter(seg_parent)))
 
             elif start_segment != None:
                 raise ValidationException("Two segments with parent id "
                                           "as -1 (i.e no parent): {0} and {1}".
                                           format(formatter(start_segment)
-                                                     , formatter(segment)))
+                                                 , formatter(segment)))
             else:
                 start_segment = segment
-                logger.info("starting segment is {0}".format(formatter(segment)))
+                logger.info(
+                    "starting segment is {0}".format(formatter(segment)))
 
     except ValidationException as v:
         logger.error("Validation error: {0}".format(v))
@@ -46,38 +47,36 @@ def validate_morphology(segments):
 
 # Generate morphology object from given cell
 def generate_morph_object(cell):
-
     # sort segment list in ascending order of id
     sorted_segments = sorted(cell.morphology.segments, key=lambda x: x.id)
     logger.info("Sorted segments are: {0}".format(formatter(sorted_segments)))
+    points = get_tuple_points(sorted_segments)
 
+    return Morphology.from_points(points, spherical_soma=False)
+
+
+# Generate tuple of points from segments
+def get_tuple_points(segments):
     # tuple generation
     points = ()
 
     # validate morphology
-    validate_morphology(sorted_segments)
+    validate_morphology(segments)
 
     # generate initial tuple
-    seg = sorted_segments[0]
-    point = (seg.id, seg.name, seg.proximal.x \
-                 , seg.proximal.y, \
-             seg.proximal.z, seg.proximal.diameter, \
-             -1 if seg.parent == None else seg.parent.segments)
+    seg = segments[0]
+    point = (seg.id, seg.name, seg.proximal.x , seg.proximal.y, seg.proximal.z, seg.proximal.diameter, -1 if seg.parent == None else seg.parent.segments)
     points = points + (point,)
 
     # iterate over morphology segments
-    for segment in sorted_segments:
-        segment_type = get_segment_type(segment)
-        point = (segment.id + 1, segment.name, segment.distal.x \
-                     , segment.distal.y, \
-                 segment.distal.z, segment.distal.diameter, \
-                 0 if segment.parent == None else segment.parent.segments + 1)
+    for segment in segments:
+        point = (segment.id + 1, segment.name, segment.distal.x , segment.distal.y, segment.distal.z, segment.distal.diameter, 0 if segment.parent == None else segment.parent.segments + 1)
         points = points + (point,)
 
     logger.info("Sequence of points sent to `from_points` function: \n{0}"
                 .format(formatter(points)))
 
-    return Morphology.from_points(points, spherical_soma=False)
+    return points
 
 
 # Return morphology object present in cell specified by cell_id
@@ -88,17 +87,17 @@ def load_morph_from_cells(cells, cell_id=None):
         if (cell_id == cell.id):
             return generate_morph_object(cell)
 
-    err= ("The cell id you specified {0} doesn't exist."
-          " Present cell id's are:\n {1}".format( formatter(cell_id),
-          formatter([cell.id for cell in cells])))
-    logger.error('Value error: %s'% err)
+    err = ("The cell id you specified {0} doesn't exist."
+           " Present cell id's are:\n {1}".format(formatter(cell_id),
+                                                  formatter([cell.id for cell in
+                                                             cells])))
+    logger.error('Value error: %s' % err)
 
     raise ValueError(err)
 
 
 # Returns final morphology object
 def load_morphology(nml_file, cell_id=None):
-
     # Generate absolute path if not provided already
     nml_file = abspath(nml_file)
 
@@ -106,7 +105,7 @@ def load_morphology(nml_file, cell_id=None):
     validate_neuroml2(nml_file)
     logger.info("Validated provided .nml file")
 
-    # Return morphology
+    # Load nml file
     doc = loaders.NeuroMLLoader.load(nml_file)
     logger.info("Loaded morphology file from: {0}".format(nml_file))
 
@@ -115,7 +114,8 @@ def load_morphology(nml_file, cell_id=None):
             return load_morph_from_cells(doc.cells, cell_id=cell_id)
         morphologies = {}
         for cell in doc.cells:
-            morphologies[cell.id] = load_morph_from_cells(doc.cells, cell_id=cell.id)
+            morphologies[cell.id] = load_morph_from_cells(doc.cells,
+                                                          cell_id=cell.id)
         return morphologies
 
     elif size(doc.cells) == 1:
